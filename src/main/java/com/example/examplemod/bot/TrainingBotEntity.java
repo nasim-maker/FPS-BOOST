c805d5de9f75ca24c34335cd44b9d19a8a2fe47a
nasim-maker <oumaramel9@gmail.com>
2026-07-07 09:22:43 +0100

    Update TrainingBotEntity.java

--- src/main/java/com/example/examplemod/bot/TrainingBotEntity.java
+++ src/main/java/com/example/examplemod/bot/TrainingBotEntity.java
@@ -1,4 +1,18 @@
 package com.example.examplemod.bot;
 
 public class TrainingBotEntity {
+
+    private final TrainingBot bot;
+
+    public TrainingBotEntity() {
+        this.bot = new TrainingBot();
+    }
+
+    public void spawn() {
+        bot.spawn();
+    }
+
+    public TrainingBot getBot() {
+        return bot;
+    }
 }

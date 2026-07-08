2a7eeb77e3f0de30651cac8f27b646acb51a1053
nasim-maker <oumaramel9@gmail.com>
2026-07-08 15:00:34 +0100

    Update MovementAI.java

--- src/main/java/com/example/examplemod/bot/ai/MovementAI.java
+++ src/main/java/com/example/examplemod/bot/ai/MovementAI.java
@@ -4,21 +4,13 @@ import com.example.examplemod.bot.TrainingBotEntity;
 
 public class MovementAI {
 
-    public MovementAI() {
-
-    }
-
     public void tick(TrainingBotEntity bot) {
 
-        // إذا ما فيه هدف لا يسوي شيء
         if (bot.getTarget() == null) {
+            bot.setSprinting(false);
             return;
         }
 
-        // هنا سنضيف الذكاء لاحقًا:
-        // - Sprint
-        // - Jump
-        // - Strafing
-        // - Pathfinding
+        bot.setSprinting(true);
     }
 }

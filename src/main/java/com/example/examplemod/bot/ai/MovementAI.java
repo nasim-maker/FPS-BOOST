440c54ac477bdc5134be74c8b4af19167c4430cf
nasim-maker <oumaramel9@gmail.com>
2026-07-08 14:36:12 +0100

    Update MovementAI.java

--- src/main/java/com/example/examplemod/bot/ai/MovementAI.java
+++ src/main/java/com/example/examplemod/bot/ai/MovementAI.java
@@ -1,1 +1,11 @@
+package com.example.examplemod.bot.ai;
 
+import com.example.examplemod.bot.TrainingBotEntity;
+
+public class MovementAI {
+
+    public void tick(TrainingBotEntity bot) {
+
+    }
+
+}
